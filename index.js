113dffa1f7e9c1aac94f0127c2cbfb329bac6641
christhian vieira <christhian11rv@hotmail.com>
2023-03-02 11:16:09 -0300

    Refactoring and new schemas and resolvers

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const { gql, ApolloServer } = require('apollo-server');
+const { ApolloServer } = require('apollo-server');
 const graphql = require('./src/graphql');
 
 const server = new ApolloServer({
-    ...graphql
+    ...graphql,
+    formatError: (err) => {
+        return new Error(err.message);
+    }
 });
 
 server.listen().then(({ url }) => console.log(`Servidor rodando em ${url}`));
--- src/graphql/modules/User/resolvers.js
+++ src/graphql/modules/User/resolvers.js
@@ -1,5 +1,14 @@
 const db = require('../../../db');
 
+function generateId(list) {
+    const lastItem = list[list.length - 1];
+
+    if (!!lastItem)
+        return lastItem.id + 1;
+
+    return 1;
+}
+
 module.exports = {
     User: {
         profile(user) {
@@ -7,11 +16,64 @@ module.exports = {
         }
     },
     Query: {
-        user(obj, args) {
-            return db.users.find(u => u.id == args.id);
+        user(obj, { filter: { id, email }, }) {
+            const field = !!id ? id : email;
+            const fieldName = !!id ? 'id' : 'email';
+
+            return db.users.find(u => u[fieldName] == field);
         },
         users() {
             return db.users;
         }
+    },
+    Mutation: {
+        createUser(obj, { data, }) {
+            const { email, } = data;
+            
+            const userExists = db.users.some(u => u.email === email);
+
+            if (!!userExists)
+                throw new Error(`There is already a user with email ${email}`);
+
+            const newUser = {
+                ...data,
+                id: generateId(db.users),
+                profile: 2,
+            };
+
+            db.users.push(newUser);
+            return newUser;
+        },
+
+        updateUser(obj, { id, data, }) {
+            const user = db.users.find(u => u.id === id);
+
+            if (!user)
+                throw new Error(`User with id ${id} not exists`);
+
+            const userIndex = db.users.findIndex(u => u.id === id);
+
+            const newUser = {
+                ...user,
+                ...data,
+            };
+
+            db.users[userIndex] = newUser;
+
+            return newUser;
+        },
+        deleteUser(obj, { filter: { id, email }, }) {
+            const field = !!id ? id : email;
+            const fieldName = !!id ? 'id' : 'email';
+
+            const userFound = db.users.find(u => u[fieldName] === field);
+
+            if (!userFound)
+                return false;
+
+            db.users = db.users.filter(u => u[fieldName] !== field);
+
+            return true;
+        },
     }
 }
